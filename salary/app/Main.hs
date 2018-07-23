c724ad6491304d8ef24fc950ccbb46bd8544b0fc
Leon Nikitin <nlv@lab321.ru>
2018-07-23 23:00:46 +0600

    показывает people через api

--- salary/app/Main.hs
+++ salary/app/Main.hs
@@ -1,30 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
-
-import Data.Text (Text)
 
 import Network.Wai.Handler.Warp
 
 import Data
 import Api
 
-import Database.Beam
-import Database.Beam.Postgres
-
-
 main :: IO ()
-main = someFunc2
+main = run 8081 salaryApp
 
-someFunc2 :: IO ()
-someFunc2 = run 8081 salaryApp
-
-someFunc :: IO ()
-someFunc = do
-        conn <- connectPostgreSQL "postgresql://nlv@localhost/salary"
-        runBeamPostgresDebug putStrLn conn $ do
-            people <- runSelectReturningList $ select allPeople
-            mapM_ (liftIO . putStrLn . show) people
-        where salaryDb :: DatabaseSettings be SalaryDb
-              salaryDb = defaultDbSettings
-
-              allPeople = all_ (_salaryPeople salaryDb)
--- salary/src/Api.hs
+++ salary/src/Api.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
-{- LANGUAGE TypeInType #-}
+{-# LANGUAGE OverloadedStrings #-}
+{- LANGUAGE TypeInType #-} -- FIXME type PeopleAPI = "people" :> Get '[JSON] [People] Не работает
 
 module Api
     ( salaryApp
@@ -10,14 +11,14 @@ module Api
 import Servant.API
 import Servant.Server
 
--- FIXME type PeopleAPI = "people" :> Get '[JSON] [People] Не работает
 import Database.Beam
+import Database.Beam.Postgres
 
 import Data.Proxy
 import Data.Aeson
+import Data.Text (Text)
 
 import Data
-
 
 instance ToJSON (PeopleT Identity)
 
@@ -26,7 +27,14 @@ type PeopleAPI = "people" :> Get '[JSON] [PeopleT Identity]
 
 type SalaryAPI = PeopleAPI
 
-handlePeople = return []
+handlePeople = liftIO $ do
+        conn <- connectPostgreSQL "postgresql://nlv@localhost/salary"
+        runBeamPostgresDebug putStrLn conn $ do
+            runSelectReturningList $ select allPeople
+        where salaryDb :: DatabaseSettings be SalaryDb
+              salaryDb = defaultDbSettings
+
+              allPeople = all_ (_salaryPeople salaryDb)
 
 salaryServer :: Server SalaryAPI
 salaryServer = handlePeople
